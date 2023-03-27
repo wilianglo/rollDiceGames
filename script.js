8009f8f1d9025ae652b8789859389cd6793a4e25
wilianglo <will.d.joyboy@gmail.com>
2023-03-27 17:37:48 +0700

    refactoring the codes

--- script.js
+++ script.js
@@ -1,115 +1,143 @@
 'use strict';
 //Selecting Elemen
+alert(
+  `The role : if dice "1" switch player & current score - 25, player reach score 100 is winner`
+);
 const score0EL = document.querySelector('#score--0');
 const score1EL = document.getElementById('score--1');
 const diceEL = document.querySelector('.dice');
-const current0EL = document.querySelector('#current--0');
-const current1EL = document.querySelector('#current--1');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
 const player0 = document.querySelector('.player--0');
 const player1 = document.querySelector('.player--1');
 const btnNew = document.querySelector('.btn--new');
-//Starting Condition
-score0EL.textContent = 0;
-score1EL.textContent = 0;
-diceEL.classList.add('hidden');
-let tempTotal0 = 0;
-let tempTotal1 = 0;
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnCurrentLabel = document.querySelector('.current-label');
-let current = 0;
-let score0 = 0;
-let score1 = 0;
-let win = false;
-function checkTotal(total, win) {
-  if (total >= 100) {
-    document.querySelector(`.win${win}`).textContent = 'WINNER';
-    document.querySelector(`.win${win}`).style.fontSize = '3rem';
-    return true;
-  }
-}
-function resetAll() {
+let activePlayer, win, holdScore, currentScore, score, total;
+
+//Starting Condition
+const resetAll = function () {
+  activePlayer = 0;
+  win = false;
+  holdScore = [0, 0];
+  currentScore = [0, 0];
+  score = [0, 0];
+  total = [0, 0];
   score0EL.textContent = 0;
   score1EL.textContent = 0;
   current0EL.textContent = 0;
   current1EL.textContent = 0;
-  current = 0;
   player0.classList.add('player--active');
   player1.classList.remove('player--active');
-  win = false;
-  tempTotal0 = 0;
-  tempTotal1 = 0;
-  score0 = 0;
-  score1 = 0;
+  player0.classList.remove('player--winner');
+  player1.classList.remove('player--winner');
   diceEL.classList.add('hidden');
-  document.querySelector('.win0').textContent = 'Current';
-  document.querySelector('.win1').textContent = 'Current';
-  document.querySelector('.win0').style.fontSize = '1.7rem';
-  document.querySelector('.win1').style.fontSize = '1.7rem';
+  document.querySelector('.winner0').classList.add(`hidden`);
+  document.querySelector('.winner1').classList.add(`hidden`);
+};
+resetAll();
+function checkTotal(total, win) {
+  if (total >= 100) {
+    document.querySelector(`.player--${win}`).classList.add(`player--winner`);
+    document.querySelector(`.winner${win}`).classList.toggle('hidden');
+    // document.querySelector(`.win${win}`).style.fontSize = '3rem';
+    diceEL.classList.add('hidden');
+    return true;
+  }
 }
+const switchPlayer = function () {
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.toggle('player--active');
+};
 btnNew.addEventListener('click', resetAll);
+//Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (!win) {
-    let dadu = Math.trunc(Math.random() * 6 + 1);
-    if (dadu === 1 && current === 0) {
-      current = 1;
-      player0.classList.remove('player--active');
-      player1.classList.add('player--active');
-    } else if (dadu === 1 && current === 1) {
-      current = 0;
-      player0.classList.add('player--active');
-      player1.classList.remove('player--active');
-    }
-
-    if (current === 0 && dadu !== 1) {
-      tempTotal0 += dadu;
-      current0EL.textContent = tempTotal0;
-      let total0 = score0 + tempTotal0;
-      win = checkTotal(total0, 0);
-      // if (total0 >= 100) {
-      //   document.querySelector('.win0').textContent = 'WINNER';
-      //   document.querySelector('.win0').style.fontSize = '3rem';
-      //   win = true;
-      //   //   resetAll();
-      // }
-    } else if (current === 1 && dadu !== 1) {
-      tempTotal1 += dadu;
-      current1EL.textContent = tempTotal1;
-      let total1 = score1 + tempTotal1;
-      win = checkTotal(total1, 1);
-      // if (total1 >= 100) {
-      //   document.querySelector('.win1').textContent = 'WINNER';
-      //   document.querySelector('.win1').style.fontSize = '3rem';
-      //   win = true;
-      //   //   resetAll();
-      // }
-    }
+    //1. generating a random dice roll
+    let dadu = Math.trunc(Math.random() * 6) + 1;
+    //2. display dice
     diceEL.classList.remove('hidden');
     diceEL.src = `dice-${dadu}.png`;
+    //3. check for rolled 1
+    if (dadu !== 1) {
+      currentScore[`${activePlayer}`] += dadu;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore[`${activePlayer}`];
+      total[`${activePlayer}`] =
+        holdScore[`${activePlayer}`] + currentScore[`${activePlayer}`];
+      win = checkTotal(total[`${activePlayer}`], `${activePlayer}`);
+    } else {
+      currentScore[`${activePlayer}`] =
+        currentScore[`${activePlayer}`] < 25
+          ? currentScore[`${activePlayer}`]
+          : (currentScore[`${activePlayer}`] -= 25);
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore[`${activePlayer}`];
+      switchPlayer();
+    }
+    // if (dadu === 1 && activePlayer === 0) {
+    //   activePlayer = 1;
+    //   player0.classList.remove('player--active');
+    //   player1.classList.add('player--active');
+    // } else if (dadu === 1 && current === 1) {
+    //   activePlayer = 0;
+    //   player0.classList.add('player--active');
+    //   player1.classList.remove('player--active');
+    // }
+
+    // if (activePlayer === 0 && dadu !== 1) {
+    //   tempTotal0 += dadu;
+    //   current0EL.textContent = tempTotal0;
+    //   let total0 = score0 + tempTotal0;
+    //   win = checkTotal(total0, 0);
+    // } else if (activePlayer === 1 && dadu !== 1) {
+    //   tempTotal1 += dadu;
+    //   current1EL.textContent = tempTotal1;
+    //   let total1 = score1 + tempTotal1;
+    //   win = checkTotal(total1, 1);
+    // }
   }
 });
 btnHold.addEventListener('click', function () {
+  // if (!win) {
+  //   if (activePlayer === 0) {
+  //     holdScore[`${activePlayer}`] += currentScore[`${activePlayer}`];
+  //     score0EL.textContent = holdScore[`${activePlayer}`];
+  //     currentScore[`${activePlayer}`] = 0;
+  //     total[`${activePlayer}`] = holdScore[`${activePlayer}`];
+  //     win = checkTotal(total[`${activePlayer}`], `${activePlayer}`);
+  //     current0EL.textContent = currentScore[`${activePlayer}`];
+  //     // win = score0 >= 100 ? true : false;
+  //     switchPlayer();
+  //   } else if (activePlayer === 1) {
+  //     holdScore[`${activePlayer}`] += currentScore[`${activePlayer}`];
+  //     score1EL.textContent = holdScore[`${activePlayer}`];
+  //     currentScore[`${activePlayer}`] = 0;
+  //     total[`${activePlayer}`] = holdScore[`${activePlayer}`];
+  //     win = checkTotal(total[`${activePlayer}`], `${activePlayer}`);
+  //     current1EL.textContent = currentScore[`${activePlayer}`];
+  //     // win = score0 >= 100 ? true : false;
+  //     switchPlayer();
+  //   }
+  // }
   if (!win) {
-    if (current === 0) {
-      current = 1;
-      score0 += tempTotal0;
-      score0EL.textContent = score0;
-      tempTotal0 = 0;
-      win = checkTotal(score0, 0);
-      current0EL.textContent = tempTotal0;
-      // win = score0 >= 100 ? true : false;
-      player0.classList.remove('player--active');
-      player1.classList.add('player--active');
-    } else if (current === 1) {
-      current = 0;
-      score1 += tempTotal1;
-      score1EL.textContent = score1;
-      tempTotal1 = 0;
-      win = checkTotal(score1, 1);
-      // win = score1 >= 100 ? true : false;
-      current1EL.textContent = tempTotal1;
-      player0.classList.add('player--active');
-      player1.classList.remove('player--active');
-    }
+    holdScore[`${activePlayer}`] += currentScore[`${activePlayer}`];
+    document.querySelector(`#score--${activePlayer}`).textContent =
+      holdScore[`${activePlayer}`];
+    currentScore[`${activePlayer}`] = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore[`${activePlayer}`];
+    total[`${activePlayer}`] = holdScore[`${activePlayer}`];
+    win = checkTotal(total[`${activePlayer}`], `${activePlayer}`)
+      ? true
+      : switchPlayer();
+    // win = score0 >= 100 ? true : false;
+    // if (!win) switchPlayer();
   }
 });
---
+++ scriptmyVersion.js
@@ -0,0 +1,116 @@
+'use strict';
+//Selecting Elemen
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+const current0EL = document.getElementById('current--0');
+const current1EL = document.getElementById('current--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
+const btnNew = document.querySelector('.btn--new');
+//Starting Condition
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
+let tempTotal0 = 0;
+let tempTotal1 = 0;
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const btnCurrentLabel = document.querySelector('.current-label');
+let current = 0;
+let score0 = 0;
+let score1 = 0;
+let win = false;
+const score = [];
+function checkTotal(total, win) {
+  if (total >= 100) {
+    document.querySelector(`.win${win}`).textContent = 'WINNER';
+    document.querySelector(`.win${win}`).style.fontSize = '3rem';
+    return true;
+  }
+}
+function resetAll() {
+  score0EL.textContent = 0;
+  score1EL.textContent = 0;
+  current0EL.textContent = 0;
+  current1EL.textContent = 0;
+  current = 0;
+  player0.classList.add('player--active');
+  player1.classList.remove('player--active');
+  win = false;
+  tempTotal0 = 0;
+  tempTotal1 = 0;
+  score0 = 0;
+  score1 = 0;
+  diceEL.classList.add('hidden');
+  document.querySelector('.win0').textContent = 'Current';
+  document.querySelector('.win1').textContent = 'Current';
+  document.querySelector('.win0').style.fontSize = '1.7rem';
+  document.querySelector('.win1').style.fontSize = '1.7rem';
+}
+btnNew.addEventListener('click', resetAll);
+btnRoll.addEventListener('click', function () {
+  if (!win) {
+    let dadu = Math.trunc(Math.random() * 6) + 1;
+    if (dadu === 1 && current === 0) {
+      current = 1;
+      player0.classList.remove('player--active');
+      player1.classList.add('player--active');
+    } else if (dadu === 1 && current === 1) {
+      current = 0;
+      player0.classList.add('player--active');
+      player1.classList.remove('player--active');
+    }
+
+    if (current === 0 && dadu !== 1) {
+      tempTotal0 += dadu;
+      current0EL.textContent = tempTotal0;
+      let total0 = score0 + tempTotal0;
+      win = checkTotal(total0, 0);
+      // if (total0 >= 100) {
+      //   document.querySelector('.win0').textContent = 'WINNER';
+      //   document.querySelector('.win0').style.fontSize = '3rem';
+      //   win = true;
+      //   //   resetAll();
+      // }
+    } else if (current === 1 && dadu !== 1) {
+      tempTotal1 += dadu;
+      current1EL.textContent = tempTotal1;
+      let total1 = score1 + tempTotal1;
+      win = checkTotal(total1, 1);
+      // if (total1 >= 100) {
+      //   document.querySelector('.win1').textContent = 'WINNER';
+      //   document.querySelector('.win1').style.fontSize = '3rem';
+      //   win = true;
+      //   //   resetAll();
+      // }
+    }
+    diceEL.classList.remove('hidden');
+    diceEL.src = `dice-${dadu}.png`;
+  }
+});
+btnHold.addEventListener('click', function () {
+  if (!win) {
+    if (current === 0) {
+      current = 1;
+      score0 += tempTotal0;
+      score0EL.textContent = score0;
+      tempTotal0 = 0;
+      win = checkTotal(score0, 0);
+      current0EL.textContent = tempTotal0;
+      // win = score0 >= 100 ? true : false;
+      player0.classList.remove('player--active');
+      player1.classList.add('player--active');
+    } else if (current === 1) {
+      current = 0;
+      score1 += tempTotal1;
+      score1EL.textContent = score1;
+      tempTotal1 = 0;
+      win = checkTotal(score1, 1);
+      // win = score1 >= 100 ? true : false;
+      current1EL.textContent = tempTotal1;
+      player0.classList.add('player--active');
+      player1.classList.remove('player--active');
+    }
+  }
+});
